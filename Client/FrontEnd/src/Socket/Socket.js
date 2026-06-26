5db6175a7f67316bf84af5f177d6130567a3e790
PalParag7781 <palparag7781@gmail.com>
2026-06-26 22:54:30 +0530

    Update Socket.js

--- Client/FrontEnd/src/Socket/Socket.js
+++ Client/FrontEnd/src/Socket/Socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 export let socket = null
 export const socketConnect = (userId) => {
-    socket = io("http://localhost:5000", {
+    socket = io("https://baatein-realtime-chat-backend.onrender.com", {
         withCredentials: true,
         query: {
             userId
