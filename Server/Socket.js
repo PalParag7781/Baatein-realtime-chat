e4ca46c3097c64b516734e6a225b91fe96f36d22
PalParag7781 <palparag7781@gmail.com>
2026-06-26 23:03:21 +0530

    Update Socket.js

--- Server/Socket.js
+++ Server/Socket.js
@@ -9,7 +9,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://baatein-realtime-chat-frontend.onrender.com",
         credentials: true
 
     }
