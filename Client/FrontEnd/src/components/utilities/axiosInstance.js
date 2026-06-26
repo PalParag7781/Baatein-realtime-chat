b77e73e1ea2a9a8fec7335cd56f2b45c48da9572
PalParag7781 <palparag7781@gmail.com>
2026-06-26 22:48:07 +0530

    Update axiosInstance.js

--- Client/FrontEnd/src/components/utilities/axiosInstance.js
+++ Client/FrontEnd/src/components/utilities/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const DB_URL = import.meta.env.VITE_DB_URL;
+const DB_URL = "https://baatein-realtime-chat-backend.onrender.com";
 
 export const axiosInstance = axios.create({
     baseURL: DB_URL,
     withCredentials: true,
     headers: {
-        ContentType: "application/json",
+        Content-Type: "application/json",
     },
 });
