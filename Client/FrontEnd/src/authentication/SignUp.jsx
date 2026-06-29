05cee79252ed0da17666a4f6b405f314b1b7c71b
PalParag7781 <palparag7781@gmail.com>
2026-06-29 23:06:10 +0530

    Update SignUp.jsx

--- Client/FrontEnd/src/authentication/SignUp.jsx
+++ Client/FrontEnd/src/authentication/SignUp.jsx
@@ -31,10 +31,11 @@ function SignUp() {
     if (response.payload.accessToken) {
       localStorage.setItem("accessToken", response.payload.accessToken);
     }
+     if (registerUserThunk.fulfilled.match(response)) {
+      navigate("/login", { replace: true });
+    }
   };
-  useEffect(() => {
-    if (isAuthenticated) navigate("/login", { replace: true });
-  }, [isAuthenticated, navigate]);
+ 
   return (
     <>
       <div className=" flex items-center justify-center py-4 mt-5">
